5631d83413ff55e197356d111ae930089ce27148
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-08-28 13:53:27 +0300

    Register View

---
+++ app/src/components/Views/Register/RegisterView.js
@@ -0,0 +1,28 @@
+import { RegisterStyle } from "../../../styles/ViewsStyles/RegisterStyle/Register.style";
+
+export default function RegisterView() {
+
+
+    return (
+        <RegisterStyle>
+            <h2>Register</h2>
+            <form action="#" method="post">
+                <label htmlFor="username">Username:</label>
+                <input type="text" id="username" name="username" required="" />
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required="" />
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" name="password" required="" />
+                <label htmlFor="confirm-password">Confirm Password:</label>
+                <input
+                    type="password"
+                    id="confirm-password"
+                    name="confirm-password"
+                    required=""
+                />
+                <button type="submit">Register</button>
+            </form>
+        </RegisterStyle>
+
+    )
+}
