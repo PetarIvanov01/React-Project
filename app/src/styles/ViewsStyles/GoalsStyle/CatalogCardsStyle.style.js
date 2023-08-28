4249f7c2ef8fd71de673b7767b47784d28d640ed
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-08-28 13:49:32 +0300

    Styles for Catalog

---
+++ app/src/styles/ViewsStyles/GoalsStyle/CatalogCardsStyle.style.js
@@ -0,0 +1,17 @@
+import { styled } from "styled-components";
+import { CardStyle } from "../HomeStyle/CardStyle";
+
+
+
+export const CatalogCardsStyle = styled.div`
+
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 6em;
+`
+
+export const CardsStyle = styled(CardStyle)`
+    margin: 0;
+`;
---
+++ app/src/styles/ViewsStyles/GoalsStyle/Pagination.style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const PaginationStyle = styled.div`
+
+    display: flex;
+    justify-content: center;
+    margin-top: auto;
+`
