0d0c96863461f4a8da268c891c6ef52aa81de634
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-02 14:28:32 +0300

    Update: LoginView Component provides users with a form to log in by entering their email and password.

--- app/src/components/Views/Login/LoginView.js
+++ app/src/components/Views/Login/LoginView.js
@@ -1,18 +1,39 @@
 import { LoginContainerStyle } from "../../../styles/ViewsStyles/LoginStyle/Login.style";
+import { useState } from "react";
+import useAuthForm from "../../../hooks/authForm";
 
 export default function LoginView() {
 
+    const [fields, setFields] = useState({
+        email: '',
+        password: '',
+        err: ''
+    });
+
+
+    const { onChangeHandler, onSubmitHandler } = useAuthForm(fields, setFields);
 
     return (
         <LoginContainerStyle>
 
             <h2>Login</h2>
-
-            <form action="#" method="post">
+            {fields.err ? <h3>{fields.err}</h3> : ''}
+            <br />
+            <form onSubmit={onSubmitHandler} action="#" method="post">
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required="" />
+                <input type="email"
+                    id="email"
+                    name="email"
+                    required=""
+                    onChange={onChangeHandler}
+                    value={fields.email} />
                 <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" required="" />
+                <input type="password"
+                    id="password"
+                    name="password"
+                    required=""
+                    onChange={onChangeHandler}
+                    value={fields.password} />
                 <button type="submit">Login</button>
             </form>
 
