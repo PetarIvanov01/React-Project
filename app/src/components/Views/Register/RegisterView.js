cc31fa00d786f776fb83e28081fd2bc541abc2e9
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-02 14:29:26 +0300

    Update RegisterView Component

--- app/src/components/Views/Register/RegisterView.js
+++ app/src/components/Views/Register/RegisterView.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
+import useAuthForm from "../../../hooks/authForm";
 import { RegisterStyle } from "../../../styles/ViewsStyles/RegisterStyle/Register.style";
-import { register } from "../../../api/goalsApi";
 
 export default function RegisterView() {
 
@@ -11,35 +11,16 @@ export default function RegisterView() {
         err: ''
     });
 
-    async function onSubmitHandler(e) {
-        e.preventDefault();
-        try {
 
-            if (Object.entries(fields).some(([k, v]) => (v === '' && k !== 'err'))) {
-                throw new Error('All fields are required!');
-            }
-            if (fields.password !== fields.confirmPassword) {
-                throw new Error('Password are not the same!');
-            }
+    const { onChangeHandler, onSubmitHandler } = useAuthForm(fields, setFields);
 
-            await register(fields.email, fields.password)
-
-        } catch (error) {
-            setFields((state) => ({ ...state, err: error.message }))
-            console.error(error);
-        }
-    }
-
-    function onChangeHandler(e) {
-        setFields((state) => ({ ...state, [e.target.name]: e.target.value, err: '' }))
-    }
 
     return (
         <RegisterStyle>
             <h2>Register</h2>
             {fields.err ? <h3>{fields.err}</h3> : ''}
             <br />
-            <form onSubmit={onSubmitHandler}>
+            <form onSubmit={(e) => onSubmitHandler(e, true)}>
                 <label htmlFor="email">Email:</label>
                 <input type="email" id="email" name="email" required="" onChange={onChangeHandler} value={fields.email} />
                 <label htmlFor="password">Password:</label>
