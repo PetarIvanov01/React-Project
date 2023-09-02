7a20820921d2148a51cbd017233383cafff81798
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-02 14:26:50 +0300

    Update:  Updated Header Component with Authentication Links:

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -1,9 +1,11 @@
+import { useAuth } from "../../contexts/auth";
 import { HeaderStyle, SignInStyle } from "../../styles/Header.style";
 import { Link } from "react-router-dom";
 
 export default function Header() {
 
-
+    const { user } = useAuth();
+    
     return (
         <HeaderStyle>
             <nav>
@@ -16,18 +18,19 @@ export default function Header() {
                     <Link to="/goals">Goals</Link>
                 </div>
 
-                {false ?
-
-                    <SignInStyle>
-                        <Link to="/login" >Sign In</Link>
-                        <Link to="/register" >Sign Up</Link>
-                    </SignInStyle> :
+                {user ?
 
                     <SignInStyle >
                         <Link to="/profile/:userId" >Profile</Link>
                         <Link to="/create" >Create</Link>
                         <Link to="/logout" >Logout</Link>
-                    </SignInStyle>}
+                    </SignInStyle> :
+
+                    <SignInStyle>
+                        <Link to="/login" >Sign In</Link>
+                        <Link to="/register" >Sign Up</Link>
+                    </SignInStyle>
+                }
 
             </nav>
         </HeaderStyle>
