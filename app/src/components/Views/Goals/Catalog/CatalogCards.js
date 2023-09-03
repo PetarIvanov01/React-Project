7d0602338426d4034e3104102427d049ecfa868c
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-03 14:11:07 +0300

    Added CatalogCardsContainer to CatalogView: Implemented the CatalogCardsContainer component in the CatalogView, which displays goal cards fetched from the GoalsProvider. A loading spinner is shown while goals are being loaded.

--- app/src/components/Views/Goals/Catalog/CatalogCards.js
+++ app/src/components/Views/Goals/Catalog/CatalogCards.js
@@ -1,14 +1,20 @@
+import { useCatalogGoals } from "../../../../contexts/catalog";
+import LoadingSpinner from "../../../../styles/LoadingSpinner.style";
 import { CatalogCardsStyle } from "../../../../styles/ViewsStyles/GoalsStyle/CatalogCardsStyle.style"
-
 import Card from "./Card";
 
 export default function CatalogCardsContainer() {
+
+    const { goals } = useCatalogGoals()
+
 
     return (
 
         <CatalogCardsStyle >
 
-            <Card />
+            {goals.length !== 0 ? goals.map(g => <Card key={g._id} {...g} />) 
+            : 
+            <LoadingSpinner />}
 
         </CatalogCardsStyle >
 
