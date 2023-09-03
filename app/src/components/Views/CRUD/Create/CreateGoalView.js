9f8f4be1586047a5fedd0f702e6da3188b70afbd
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-03 14:09:43 +0300

    Refactor CreateGoalView Component. Added functionallity for creating a goal

--- app/src/components/Views/CRUD/Create/CreateGoalView.js
+++ app/src/components/Views/CRUD/Create/CreateGoalView.js
@@ -1,23 +1,54 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { CreateContainerStyle } from "../../../../styles/ViewsStyles/CRUDStyle/Create.style";
+import { createGoal } from "../../../../api/goalsApi";
 
 export default function CreateGoalView() {
 
+    const [goal, setGoalData] = useState({
+        title: '',
+        image: '',
+        description: ''
+    })
+
+    const navigate = useNavigate();
+
+    function onChangeHandler(e) {
+        setGoalData((state) => ({ ...state, [e.target.name]: e.target.value }))
+    }
+
+    async function onSubmitHandler(e) {
+        e.preventDefault();
+
+        try {
+
+            await createGoal(goal);
+
+            //Todo navigate to the person profile
+            navigate('/')
+
+        } catch (error) {
+            alert(error.message);
+            return
+        }
+    }
 
     return (
 
         <CreateContainerStyle>
             <h2>Create goal</h2>
 
-            <form action="#" method="post">
+            <form onSubmit={onSubmitHandler} action="#" method="post">
 
                 <label htmlFor="title">Title:</label>
-                <input type="text" id="title" name="title" required="" />
+                <input type="text" id="title" name="title" onChange={onChangeHandler} required="" value={goal.title} />
 
                 <label htmlFor="image">ImageUrl:</label>
-                <input type="url" id="image" name="image" required="" />
-                
+                <input type="text" id="image" name="image" onChange={onChangeHandler} required="" value={goal.image} />
+
                 <label htmlFor="goal">Goal Description:</label>
-                <textarea type="text" id="goal" name="goal" required="" />
+                <textarea type="text" id="goal" name="description" onChange={onChangeHandler} required="" value={goal.goal} />
 
                 <button type="submit">Create</button>
             </form>
