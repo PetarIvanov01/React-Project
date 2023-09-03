fcaef7f20eec8de7a66a6dd8e1eaa5f85479dc65
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-03 14:08:21 +0300

    Added new endpoint to get profile details

--- app/src/api/profileApi.js
+++ app/src/api/profileApi.js
@@ -3,11 +3,15 @@ import * as api from "./requester"
 const endpoints = {
 
     create: "user/profile",
-    edit: (id) => `profile/${id}`,
+    details: (id) => `user/profile/${id}`,
+    edit: (id) => `user/profile/${id}`,
 
 };
 
 export function createProfile(data) {
-    
+
     return api.post(endpoints.create, data)
+}
+export function getProfileDetails(id) {
+    return api.get(endpoints.details(id))
 }
