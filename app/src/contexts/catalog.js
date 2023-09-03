9de04a43875690ac6d0384aea26e8584a5bb7915
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-03 14:14:41 +0300

    Added GoalsProvider and useCatalogGoals context for managing goal data in the home and catalog views"

---
+++ app/src/contexts/catalog.js
@@ -0,0 +1,38 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import { getDetails, getCatalog, getDataForHome } from '../api/goalsApi';
+
+const HomeContext = createContext();
+
+export default function GoalsProvider({ children, view }) {
+
+    const [goals, setGoalsData] = useState([]);
+
+    useEffect(() => {
+
+        if (view === 'home') {
+            getDataForHome().then(({ items }) => {
+                setGoalsData(items.results);
+            })
+        }
+        else {
+            getCatalog().then(({ items }) => {
+                setGoalsData(items.results);
+            })
+        }
+
+    }, [view])
+
+
+    return (
+        <HomeContext.Provider value={{ goals, setGoalsData }}>
+
+            {children}
+        </HomeContext.Provider>
+    )
+}
+
+export const useCatalogGoals = () => {
+    const context = useContext(HomeContext)
+
+    return context;
+}
