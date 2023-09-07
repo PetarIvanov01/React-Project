bd0411c6ce7e711ba8236a4c018d9cf71475576c
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-07 16:57:23 +0300

    Enhance TopicCards component to include owner-specific actions for editing and deleting goals

--- app/src/components/Views/Profile/Topics/TopicCard.js
+++ app/src/components/Views/Profile/Topics/TopicCard.js
@@ -1,5 +1,7 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { CardTitle, CardTopics, DescriptionParagraph, EditGoalStyle, TopicDescription, TopicsImage } from "../../../../styles/ViewsStyles/ProfileStyle/Profile.style";
+import { useAuth } from "../../../../contexts/auth";
+import { descCutter } from "../../../../util/cutter";
 
 export default function TopicCards({
     title,
@@ -9,20 +11,35 @@ export default function TopicCards({
     handleLearnMoreClick
 }) {
 
+    const { userId } = useParams();
+    const { user } = useAuth();
+
+    const isOwner = user.id === userId;
+
+    const shortDesc = descCutter(description);
 
     return (
         <CardTopics className="card-topics">
             <TopicsImage src={image} alt="" />
+
             <CardTitle>
                 <p>{title}</p>
             </CardTitle>
+
             <TopicDescription>
-                <DescriptionParagraph>{description}</DescriptionParagraph>
+                <DescriptionParagraph>{shortDesc}</DescriptionParagraph>
                 <EditGoalStyle>
-                    <Link to="#" onClick={() => handleLearnMoreClick("Topic 1")} >Learn more...</Link>
-                    <Link to={`/edit/goal/${_id}`}><img src="/imgs/svg/edit.svg" alt="Edit Icon" /></Link>
+
+                    <Link to={_id} onClick={() => handleLearnMoreClick({ image, title, description  })}  >Learn more...</Link>
+                    {isOwner ?
+                        <div className="owner-buttons">
+                            <Link to={`/edit/goal/${_id}`}><img src="/imgs/svg/edit.svg" alt="Edit Icon" /></Link>
+                            <Link to={`/delete/goal/${_id}`}><img src="/imgs/svg/delete.svg" alt="Delete Icon" /></Link>
+                        </div>
+                        : ''}
                 </EditGoalStyle>
             </TopicDescription>
+
         </CardTopics>
     )
 }
