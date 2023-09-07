a10a739b1c2efb216008ed9494130c24f863b82e
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-07 16:58:33 +0300

    Update

--- app/src/components/Views/Profile/ProfileView.js
+++ app/src/components/Views/Profile/ProfileView.js
@@ -1,18 +1,11 @@
 import { useParams } from "react-router-dom";
-import { ProfileProvider } from "../../../contexts/profile";
 import ProfileContainer from "./ProfileContainer";
 
 export default function ProfileView() {
 
     const { userId } = useParams();
 
-
     return (
-
-        <ProfileProvider userId={userId}>
-            
-            <ProfileContainer />
-
-        </ProfileProvider>
+            <ProfileContainer userId={userId } />
     );
 }
