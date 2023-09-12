34725291e97e747a2679c392fb6e8630cd74f5cd
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-12 18:48:52 +0300

    Introduce useProfileState custom hook
    Created the useProfileState custom hook to manage user profile state with structured actions for username, aboutMe, category, and avatar.
    
    This hook enhances code organization and maintainability.

---
+++ app/src/hooks/profileReducer.js
@@ -0,0 +1,82 @@
+import { useReducer } from 'react'
+
+const initialStateValue = {
+    username: '',
+    aboutMe: '',
+    categories: [],
+    avatar: ''
+};
+
+const ACTION_TYPE = {
+    USERNAME: 'username',
+    ABOUTME: 'aboutMe',
+    CATEGORY: 'category',
+    AVATAR: 'avatar'
+};
+
+function reducer(state, action) {
+
+    switch (action.type) {
+        case ACTION_TYPE.USERNAME:
+            return { ...state, username: action.payload };
+
+        case ACTION_TYPE.ABOUTME:
+            return { ...state, aboutMe: action.payload };
+
+        case ACTION_TYPE.CATEGORY:
+            return { ...state, categories: action.payload };
+
+        case ACTION_TYPE.AVATAR:
+            return { ...state, avatar: action.payload };
+
+        default:
+            return state;
+    }
+}
+
+export default function useProfileState() {
+
+    const [state, dispatch] = useReducer(reducer, initialStateValue);
+
+    const setUsername = (e) => {
+        dispatch({ type: ACTION_TYPE.USERNAME, payload: e.target.value });
+    };
+
+    const setAboutMe = (e) => {
+        dispatch({ type: ACTION_TYPE.ABOUTME, payload: e.target.value });
+    };
+
+    const toggleCategory = (e) => {
+
+        let categories = [];
+        const checkboxName = e.target.value;
+
+        if (state.categories.length >= 1) {
+            e.target.checked = false;
+            categories = state.categories.filter((name) => name !== checkboxName);
+            return dispatch({ type: ACTION_TYPE.CATEGORY, payload: categories });
+        }
+        else {
+            // (e.target.checked)
+            categories = [...state.categories, checkboxName]
+            return dispatch({ type: ACTION_TYPE.CATEGORY, payload: categories });
+        }
+        // else {
+        //     console.log('here3');
+        //     categories = state.categories.filter((name) => name !== checkboxName)
+        //     return dispatch({ type: ACTION_TYPE.CATEGORY, payload: categories });
+        // }
+    };
+
+    const chooseAvatar = (selected) => {
+        dispatch({ type: ACTION_TYPE.AVATAR, payload: selected })
+    }
+
+    return {
+        chooseAvatar,
+        setAboutMe,
+        setUsername,
+        toggleCategory,
+        state
+    }
+}
