a6d12cd5ebf2303c80918a070eb49a86af80b3fb
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-10-03 21:23:06 +0300

    - Created tests to ensure that the HomeView component renders correctly.

--- app/src/components/Views/Home/Home.test.js
+++ app/src/components/Views/Home/Home.test.js
@@ -3,13 +3,21 @@ import '@testing-library/jest-dom/extend-expect';
 import HomeView from "./HomeView"
 import { MemoryRouter } from 'react-router-dom'
 
+describe('Test for Home Page', () => {
 
-test('renders login page link', () => {
-    render(
-        <MemoryRouter>
-            <HomeView />
-        </MemoryRouter>
-    );
-    const linkElement = screen.getByText(/Share your goals/i);
-    expect(linkElement).toBeInTheDocument();
+
+    test('renders Home page', () => {
+        render(
+            <MemoryRouter>
+                <HomeView />
+            </MemoryRouter>
+        );
+        const titleElement = screen.getByText(/Share your goals/i);
+        expect(titleElement).toBeInTheDocument();
+
+        const secondTitleEl = screen.getByText(/Two of the most/i);
+        expect(secondTitleEl).toBeInTheDocument();
+    })
+
+    
 })
