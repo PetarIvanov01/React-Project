39d58ba28c6a490596686f48e82295d7c32166a7
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-10-04 20:32:51 +0300

    Update: Added new test for login component

--- app/src/components/Views/Login/Login.test.js
+++ app/src/components/Views/Login/Login.test.js
@@ -5,25 +5,39 @@ import LoginPage from "./LoginView"
 import { MemoryRouter } from 'react-router-dom'
 import AuthProvider from "../../../contexts/auth";
 
-describe('Login Page Test', () => {
 
+describe('Login Page Test', () => {
 
     test('Chekc if renders login page', () => {
         render(
-            <MemoryRouter initialEntries={['/login']}>
+            <MemoryRouter >
                 <AuthProvider>
                     <LoginPage />
                 </AuthProvider>
             </MemoryRouter>
         );
-        const linkElement = screen.getAllByText('Login', { selector: 'h2' });
-        expect(linkElement[0]).toBeInTheDocument();
+        const loginBtn = screen.getByRole('button');
+        expect(loginBtn).toBeInTheDocument();
     })
 
+    test('Inputs should be empty', () => {
+        render(
+            <MemoryRouter >
+                <AuthProvider>
+                    <LoginPage />
+                </AuthProvider>
+            </MemoryRouter>
+        )
+        const emailInpuit = screen.getByLabelText('Email:');
+        const passwordInpuit = screen.getByLabelText('Password:');
+
+        expect(emailInpuit.value).toBe('')
+        expect(passwordInpuit.value).toBe('')
+    })
 
     test('Testing login with email and password', () => {
         render(
-            <MemoryRouter initialEntries={['/login']}>
+            <MemoryRouter>
                 <AuthProvider>
                     <LoginPage />
                 </AuthProvider>
@@ -33,16 +47,63 @@ describe('Login Page Test', () => {
             const emailInpuit = screen.getByLabelText('Email:');
             const passwordInpuit = screen.getByLabelText('Password:');
 
-            userEvent.type(emailInpuit, 'pesho@abv.bg');
+            userEvent.type(emailInpuit, 'Test@m.bg');
             userEvent.type(passwordInpuit, '123456');
 
-            const submitButton = screen.getByText('Login', { selector: 'button' });
-
-            expect(emailInpuit).toHaveValue('pesho@abv.bg');
+            expect(emailInpuit).toHaveValue('Test@m.bg');
             expect(passwordInpuit).toHaveValue('123456');
-
-            userEvent.click(submitButton);
         })
 
     })
+
+    test('Test for all fields are required', async () => {
+
+        render(
+            <MemoryRouter>
+                <AuthProvider>
+                    <LoginPage />
+                </AuthProvider>
+            </MemoryRouter>
+        );
+        const emailInput = screen.getByLabelText('Email:');
+        const passwordInput = screen.getByLabelText('Password:');
+        const loginButton = screen.getByRole('button', { name: 'Login' });
+
+        emailInput.value = '';
+        passwordInput.value = ''
+
+        let error;
+
+        await act(async () => {
+            userEvent.click(loginButton);
+        })
+        error = await screen.findByRole('heading', { level: 3 });
+        expect(error.textContent).toBe('All fields are required!');
+    })
+
+    test('Test for incorrect username or password', async () => {
+
+        render(
+            <MemoryRouter>
+                <AuthProvider>
+                    <LoginPage />
+                </AuthProvider>
+            </MemoryRouter >
+        )
+        const emailInput = screen.getByLabelText('Email:');
+        const passwordInput = screen.getByLabelText('Password:');
+        const loginButton = screen.getByRole('button', { name: 'Login' });
+
+        let error;
+
+        await act(async () => {
+            userEvent.type(emailInput, 'incorect@abv.bg');
+            userEvent.type(passwordInput, 'incorect');
+            userEvent.click(loginButton);
+        })
+
+        error = await screen.findByRole('heading', { level: 3 });
+        expect(error.textContent).toBe('Incorrect username or password');
+    })
+
 })
