07140e87b4e865de08e7194326d88536a62b4dff
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-06 18:22:40 +0200

    Reorganized Routing in Main Component

--- app/src/components/Main/Main.js
+++ app/src/components/Main/Main.js
@@ -2,43 +2,35 @@ import { Route, Routes } from "react-router-dom"
 
 import { MainStyle } from "../../styles/Main.style";
 
-import HomeView from "../Views/Home/HomeView";
-import LoginView from "../Views/Login/LoginView";
-import RegisterView from "../Views/Register/RegisterView";
-import CatalogView from "../Views/Goals/Catalog/Catalog";
-import ProfileView from "../Views/Profile/ProfileView";
-import CreateGoalView from "../Views/CRUD/Create/CreateGoalView";
-import CreateProfile from "../Views/CRUD/Create/CreateProfile";
-import EditProfile from "../Views/CRUD/Edit/EditProfile";
-import EditGoalView from "../Views/CRUD/Edit/EditGoalView";
-import Logout from "../Views/Logout/LogoutFunc";
-import DeleteGoalView from "../Views/CRUD/Delete/DeleteGoalView";
+import { Suspense } from "react";
+import { SpinnerStyle } from "../../util/SuspenseSpinner";
+
+import { authRouts, privateRoutes, publicRoutes } from "../../routes";
 
 export default function Main() {
 
-    //TODO Add guards for not authorized requests
-
     return (
-
         <MainStyle>
 
-            <Routes>
-                <Route path="/" element={<HomeView />} />
-                <Route path="/goals" element={<CatalogView />} />
+            <Suspense fallback={<SpinnerStyle />}>
 
-                <Route path="/login" element={<LoginView />} />
-                <Route path="/register" element={<RegisterView />} />
-                <Route path="/create/profile" element={<CreateProfile />} />
-                <Route path="/logout" element={<Logout />} />
+                <Routes>
+                    
+                    {publicRoutes.map((route) => (
+                        <Route key={route.path} path={route.path} element={route.element()} />
+                    ))}
 
-                <Route path="/create" element={<CreateGoalView />} />
-                <Route path="/edit/goal/:goalId" element={<EditGoalView />} />
-                <Route path="/profile/:userId/*" element={<ProfileView />} />
-                <Route path="/edit/profile/:userId" element={<EditProfile />} />
-                <Route path="/delete/goal/:goalId" element={<DeleteGoalView />} />
-            </Routes>
+                    {authRouts.map((route) => (
+                        <Route key={route.path} path={route.path} element={route.element()} />
+                    ))}
 
+                    {privateRoutes.map((route) => (
+                        <Route key={route.path} path={route.path} element={route.element()} />
+                    ))}
+
+                </Routes>
+
+            </Suspense>
         </MainStyle>
-
     )
 }
