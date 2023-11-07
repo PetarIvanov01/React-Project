845c55394de510b460cb00f671a8f14be16895a0
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-07 21:38:44 +0200

    Styled Components for Error Messages and Profile Creation Box

---
+++ app/src/styles/ViewsStyles/ErrorBoxs.style.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const CreateGoalErrorBox = styled.div`
+
+    position: absolute;
+    top: 20%;
+    background-color: rgba(244, 67, 54, 0.8); 
+    color: #fff; 
+    padding: 10px;
+    margin: 10px 0;
+    border-radius: 5px;
+    font-size: 16px;
+    text-align: center;
+
+`
+
+export const AuthErrorBox = styled.div`
+    position: absolute;
+    top: 25%;
+    background-color: rgba(244, 67, 54, 0.8); 
+    color: #fff; 
+    padding: 15px 10px;
+    margin: 10px 0;
+    border-radius: 5px;
+    font-size: 1.1em;
+    text-align: center;
+`
+
+export const CreateProfileBox = styled.div`
+
+    position: absolute;
+    top: 15%; 
+    left: 50%;
+    transform: translate(-50%, -50%); 
+    background-color: rgba(155, 113, 95, 0.46);
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 5px 5px 2px rgba(0, 0, 0, 0.4);
+    text-align: center;
+   
+
+    p {
+        font-size: 1.4em;
+        color: rgba(197, 113, 95, 0.8);
+        font-family: sans-serif;
+
+    }
+
+`
