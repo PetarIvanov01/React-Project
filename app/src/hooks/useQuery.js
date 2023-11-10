1ad04cec92a48aec1a613016f5896d850c22f726
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-10 16:05:47 +0200

    Refactor useQuery hook for improved query handling

--- app/src/hooks/useQuery.js
+++ app/src/hooks/useQuery.js
@@ -2,7 +2,16 @@ import { useLocation } from "react-router-dom";
 
 export default function useQuery() {
     const params = useLocation();
-    const [query, data] = params.search.substring(1).split('=');
 
-    return { query, data }
+    const endpoint = params.search;
+    const queriesArr = params.search.substring(1).split(/=|&/);
+    const queriesObj = {}
+    
+    if (endpoint) {
+        for (let i = 0; i < queriesArr.length; i++) {
+            queriesObj[queriesArr[i]] = queriesArr[++i];
+        }
+    }
+
+    return { endpoint, queriesObj }
 }
