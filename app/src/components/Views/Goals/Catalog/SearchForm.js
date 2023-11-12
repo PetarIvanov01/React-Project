c14dd17ddf7a5b700c544fe484fdd2a7b7e79d28
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-12 13:45:10 +0200

    Integrate useDebounce hook in SearchForm component to delay the execution of search queries, enhancing user experience by reducing unnecessary API calls

--- app/src/components/Views/Goals/Catalog/SearchForm.js
+++ app/src/components/Views/Goals/Catalog/SearchForm.js
@@ -1,35 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SearchContainer, SearchInput } from "../../../../styles/ViewsStyles/GoalsStyle/Search.style";
 import { useNavigate } from "react-router-dom";
+import useDebounce from "../../../../hooks/useDebounce";
 
 export default function SearchForm() {
 
     const navigate = useNavigate();
-
     const [query, setSearchQuery] = useState({
         search: ''
     });
-    
+
+    const debouncedQuery = useDebounce(query);
+
     const onChangeHandler = (e) => {
-        setSearchQuery((state) => ({ ...state, [e.target.name]: e.target.value }));
+        setSearchQuery((state) => {
+            return { ...state, [e.target.name]: e.target.value }
+        });
     }
 
-    function onSubmitHandler(e) {
-        e.preventDefault();
-
-        if (query.search) {
-            navigate(`/goals?search=${query.search}`);
+    useEffect(() => {
+        if (debouncedQuery.search) {
+            navigate(`/goals?search=${debouncedQuery.search}`);
         }
         else {
             navigate(`/goals`);
         }
-    }
-
+    }, [debouncedQuery])
 
     return (
         <SearchContainer>
-            <form onSubmit={onSubmitHandler}>
-                <SearchInput type="text" name="search" onChange={onChangeHandler} value={query.search} placeholder="Search by name ..." />
+            <form >
+                <SearchInput
+                    type="text"
+                    name="search"
+                    onChange={onChangeHandler}
+                    value={query.search}
+                    placeholder="Search by name ..." />
             </form>
         </SearchContainer>
     )
