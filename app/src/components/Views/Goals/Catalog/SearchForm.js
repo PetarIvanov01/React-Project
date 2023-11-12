cd190b3d073e0e4144c7dceb4a57572ec40e48b9
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-12 17:35:06 +0200

    Enhance the SearchForm component by adding category filtering functionality and updating the associated styles for an improved user experience.

--- app/src/components/Views/Goals/Catalog/SearchForm.js
+++ app/src/components/Views/Goals/Catalog/SearchForm.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { SearchContainer, SearchInput } from "../../../../styles/ViewsStyles/GoalsStyle/Search.style";
+import { Container, Search, Select, SearchInput } from "../../../../styles/ViewsStyles/GoalsStyle/Search.style";
 import { useNavigate } from "react-router-dom";
 import useDebounce from "../../../../hooks/useDebounce";
 
@@ -7,6 +7,7 @@ export default function SearchForm() {
 
     const navigate = useNavigate();
     const [query, setSearchQuery] = useState({
+        filter: '',
         search: ''
     });
 
@@ -19,24 +20,37 @@ export default function SearchForm() {
     }
 
     useEffect(() => {
-        if (debouncedQuery.search) {
-            navigate(`/goals?search=${debouncedQuery.search}`);
+        if (debouncedQuery.search || debouncedQuery.filter) {
+            navigate(`/goals?category=${debouncedQuery.filter}&search=${debouncedQuery.search}`);
         }
         else {
             navigate(`/goals`);
         }
-    }, [debouncedQuery])
+    }, [debouncedQuery]);
+
 
     return (
-        <SearchContainer>
-            <form >
+        <Container>
+            <Search>
                 <SearchInput
                     type="text"
                     name="search"
                     onChange={onChangeHandler}
-                    value={query.search}
-                    placeholder="Search by name ..." />
-            </form>
-        </SearchContainer>
+                    value={query.value}
+                    placeholder="Search by name and..." />
+            </Search>
+
+            <div >
+                <Select name="filter" id="filter"
+                    value={query.filter}
+                    onChange={onChangeHandler}>
+                    <option value="">Category</option>
+                    <option value="career">Career</option>
+                    <option value="sport">Sport</option>
+                    <option value="lifestyle">Lifestyle</option>
+
+                </Select>
+            </div>
+        </Container>
     )
 }
--- app/src/styles/ViewsStyles/GoalsStyle/Search.style.js
+++ app/src/styles/ViewsStyles/GoalsStyle/Search.style.js
@@ -1,16 +1,32 @@
 import styled from 'styled-components';
 
-export const SearchContainer = styled.div`
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2em;
+
+  @media (max-width: 450px) {
+    text-align: center;
+    margin-bottom: 1em;
+    padding: 0;
+  }
+`
+export const Search = styled.div`
   padding: 64px 0;
-  width: 350px;
-  margin: 0 auto
+  width: 300px;
+  margin: 0 auto;
+
+  @media (max-width: 450px) {
+    text-align: center;
+    width: 150px;
+    padding: 32px 0;
+  }
 `;
 
 export const SearchInput = styled.input`
 
   width: 100%;
   padding: 12px 24px;
-  background-color: transparent;
   transition: transform 250ms ease-in-out;
   font-size: 14px;
   line-height: 18px;
@@ -18,7 +34,7 @@ export const SearchInput = styled.input`
   background-color: transparent;
   background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath d='M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z'/%3E%3Cpath d='M0 0h24v24H0z' fill='none'/%3E%3C/svg%3E");
   background-repeat: no-repeat;
-  background-size: 18px 18px;
+  background-size: 18px;
   background-position: 95% center;
   border-radius: 50px;
   border: 1px solid #575756;
@@ -42,3 +58,22 @@ export const SearchInput = styled.input`
     background-position: 100% center;
   }
 `;
+
+export const Select = styled.select`
+
+    padding: 0.5em 0.2em;
+    border-radius: 40px;
+    border: 1px solid #575756;
+    font: var(--font-indie);
+    background-color: transparent;
+    transition: all 250ms ease-in-out;
+    text-align: center;
+    
+    &:hover,
+    &:focus {
+      border: 1px solid transparent;
+      border-bottom: 1px solid #575756;
+      border-radius: 0;
+      background-position: 100% center;
+    }
+`
