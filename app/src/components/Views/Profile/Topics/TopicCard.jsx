bc9bc2cde78c49e10d6eab079af6696f5c60b2ab
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-20 17:12:02 +0200

    Refactored the 'TopicCards' component in the 'ProfileStyle' to include like functionality and controllers for managing likes.

--- app/src/components/Views/Profile/Topics/TopicCard.jsx
+++ app/src/components/Views/Profile/Topics/TopicCard.jsx
@@ -1,22 +1,45 @@
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { CardTitle, CardTopics, DescriptionParagraph, EditGoalStyle, TopicDescription, TopicsImage } from "../../../../styles/ViewsStyles/ProfileStyle/CardStyle.style";
 import { useAuth } from "../../../../contexts/auth";
+
+import { CardTitle, CardTopics, DescriptionParagraph, EditGoalStyle, TopicDescription, TopicsImage } from "../../../../styles/ViewsStyles/ProfileStyle/CardStyle.style";
 import { descCutter } from "../../../../util/cutter";
+
+import LikePost from "../Likes/Like";
 
 export default function TopicCards({
     title,
     image,
     description,
     _id,
+    likes,
     handleLearnMoreClick
 }) {
-
     const { userId } = useParams();
     const { user } = useAuth();
 
     const isOwner = user?.id === userId;
-
     const shortDesc = descCutter(description);
+
+    const [likeInfo, setLikeInfo] = useState({
+        isLike: likes.includes(user.id),
+        likedUsers: [...likes]
+    });
+
+    const setLikes = () => {
+
+        setLikeInfo(state => {
+            const likedUsers = [...state.likedUsers];
+
+            const newlyLikedUsers = likedUsers.includes(user.id) ?
+                likedUsers.filter(id => id !== user.id)
+                :
+                [...likedUsers, user.id];
+
+            return { ...state, isLike: !state.isLike, likedUsers: newlyLikedUsers }
+        }
+        )
+    }
 
     return (
         <CardTopics>
@@ -30,13 +53,23 @@ export default function TopicCards({
                 <DescriptionParagraph>{shortDesc}</DescriptionParagraph>
                 <EditGoalStyle>
 
-                    <Link to={_id} onClick={() => handleLearnMoreClick({ image, title, description  })}  >Learn more...</Link>
-                    {isOwner &&
-                        <div className="owner-buttons">
-                            <Link to={`/edit/goal/${_id}`}><img src="/imgs/svg/edit.svg" alt="Edit Icon" /></Link>
-                            <Link to={`/delete/goal/${_id}`}><img src="/imgs/svg/delete.svg" alt="Delete Icon" /></Link>
-                        </div>
+                    <Link to={_id} onClick={() => handleLearnMoreClick({ image, title, description })}  >Learn more...</Link>
+                    <div className="controllers">
+                        <p>Likes: {likeInfo.likedUsers.length}</p>
+                        {isOwner ?
+                            <div className="owner-buttons">
+                                <Link to={`/edit/goal/${_id}`}><img src="/imgs/svg/edit.svg" alt="Edit Icon" /></Link>
+                                <Link to={`/delete/goal/${_id}`}><img src="/imgs/svg/delete.svg" alt="Delete Icon" /></Link>
+                            </div>
+                            :
+                            <LikePost
+                                setLike={setLikes}
+                                userId={user?.id}
+                                postId={_id}
+                                isLike={likeInfo.isLike}
+                            />
                         }
+                    </div>
                 </EditGoalStyle>
             </TopicDescription>
 
