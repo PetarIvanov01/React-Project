cb85ec034dc5bca571108461def53282d4663ca3
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-22 16:09:42 +0200

    Refactor the Main component to enhance error handling by introducing the ErrorBoundary component.
    Changes made:
    1. Imported ErrorBoundary and related components for better error handling.
    
    2. Included the ErrorBoundary component around the Routes to catch and display errors.
    
    3. Added a new Route to handle cases where the requested route is not found.

--- app/src/components/Main/Main.jsx
+++ app/src/components/Main/Main.jsx
@@ -3,9 +3,14 @@ import { Route, Routes } from "react-router-dom"
 import { MainStyle } from "../../styles/layout/Main.style";
 
 import { Suspense } from "react";
-import { SpinnerStyle } from "../../util/SuspenseSpinner";
+import { SpinnerStyle } from "../SuspenseSpinner";
 
 import { authRouts, privateRoutes, publicRoutes } from "../../routes";
+
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorModalComponent from "../../pages/ErrorModals/ErrorModal";
+
+import NotFound from "../../pages/ErrorModals/NotFoundRoute";
 
 export default function Main() {
 
@@ -13,24 +18,32 @@ export default function Main() {
         <MainStyle>
 
             <Suspense fallback={<SpinnerStyle />}>
+                <ErrorBoundary
+                    FallbackComponent={ErrorModalComponent}
+                >
+                    <Routes>
 
-                <Routes>
-                    
-                    {publicRoutes.map((route) => (
-                        <Route key={route.path} path={route.path} element={route.element()} />
-                    ))}
+                        {publicRoutes.map((route) => (
+                            <Route key={route.path} path={route.path} element={route.element()} />
+                        ))}
 
-                    {authRouts.map((route) => (
-                        <Route key={route.path} path={route.path} element={route.element()} />
-                    ))}
+                        {authRouts.map((route) => (
+                            <Route key={route.path} path={route.path} element={route.element()} />
+                        ))}
 
-                    {privateRoutes.map((route) => (
-                        <Route key={route.path} path={route.path} element={route.element()} />
-                    ))}
+                        {privateRoutes.map((route) => (
+                            <Route key={route.path} path={route.path} element={route.element()} />
+                        ))}
 
-                </Routes>
+                        {<Route path="*" element={<NotFound />} />}
 
+                    </Routes>
+                </ErrorBoundary>
             </Suspense>
-        </MainStyle>
+        </MainStyle >
     )
 }
+
+
+
+
