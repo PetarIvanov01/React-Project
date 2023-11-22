54d760a0af73813ae6db9a1a086ae6b76b5db3c8
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-22 16:03:48 +0200

    Changes Made - Enhanced the error handling function to provide a more structured error object.
    - Addressed specific error types, including network errors, expired JWT tokens, and unauthorized actions.

--- app/src/util/errorChecker.js
+++ app/src/util/errorChecker.js
@@ -1,11 +1,24 @@
 const tryCatchErr = (err) => {
-    let error = err
+    
+    let error = {
+        type: err.name || err.type,
+        message: err.message
+    }
     if (err.name === 'TypeError' && error.message.includes('NetworkError')) {
-        error =  'There was a network error. Please check if the server is running.';
+        error.type = 'ServerError';
+        error.message = 'There was a network error. Please check if the server is running.';
+    }
+    else if (err.type === 'TokenExpiredError') {
+        error.type = 'TokenExpiredError';
+        error.message = 'JWT Token has expired. Please log in again to obtain a new token.';
+    }
+    else if (err.type === 'Error' && err.message.includes('Authorized')) {
+        error.type = 'NotAuthorized';
+        error.message = 'You are not authorized to perform this action. Please make sure you have the necessary permissions or contact support for assistance.';
     }
 
-    console.error(error);
-    return;
+
+    return error;
 }
 
 export default tryCatchErr
