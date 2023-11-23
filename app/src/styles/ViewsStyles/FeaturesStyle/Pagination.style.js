f1b99adf1c61644e7a6a5562a8f04922a9992045
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-23 18:36:44 +0200

    style: Update Pagination component in Home view
    - Replaced SVG icons with image assets for next and previous buttons
    - Improved the accessibility and visual appeal of the pagination controls

--- app/src/styles/ViewsStyles/FeaturesStyle/Pagination.style.js
+++ app/src/styles/ViewsStyles/FeaturesStyle/Pagination.style.js
@@ -5,4 +5,18 @@ export const PaginationStyle = styled.div`
     display: flex;
     justify-content: center;
     margin-top: auto;
+
+    a {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        transition: color 0.3s ease;
+
+        transition: transform 0.3s ease;
+
+        &:hover {
+        transform: scale(1.2);
+        }
+    }
 `
