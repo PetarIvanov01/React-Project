913d82fd18c81b67b31f7fc5c6953c50628ba517
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-29 09:09:39 +0200

    Wrapping the returned function in useCallback

--- app/src/hooks/useErrorBoundryAsync.jsx
+++ app/src/hooks/useErrorBoundryAsync.jsx
@@ -1,10 +1,11 @@
 import { useErrorBoundary } from "react-error-boundary";
 import tryCatchErr from "../util/errorChecker";
+import { useCallback } from "react";
 
 export default function useErrorBoundryAsync() {
     const { showBoundary } = useErrorBoundary();
 
-    return (err) => {
+    return useCallback((err) => {
         showBoundary(tryCatchErr(err));
-    }
+    }, [showBoundary])
 }
