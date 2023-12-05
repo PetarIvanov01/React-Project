992edc286138da16a8c27ff5fa219031d0352fdd
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-05 14:37:43 +0200

    refactor: Combine TopicsView into TopicsContainer component
    Also delete the old component

--- app/src/pages/Profile/components/Topics/Topics.jsx
+++ app/src/pages/Profile/components/Topics/Topics.jsx
@@ -1,15 +1,38 @@
-import { Topics } from "../../../../styles/ViewsStyles/ProfileStyle/Profile.style";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import TopicCardsView from "./TopicsView";
+import { Topics } from "../../../../styles/ViewsStyles/ProfileStyle/Profile.style";
+import { Cards } from "../../../../styles/ViewsStyles/ProfileStyle/Profile.style";
+import InformationBox from "../InformationBox";
+import TopicCards from "./TopicCard";
 
 export default function TopicsContainer({ goals }) {
+
+    const [showInformationBox, setShowInformationBox] = useState(false);
+    const [selectedTopic, setSelectedTopic] = useState({});
+
+    const navigate = useNavigate();
+
+    const handleLearnMoreClick = (topic) => {
+        setSelectedTopic(topic);
+        setShowInformationBox(true);
+    };
+
+    const handleCloseBox = () => {
+        navigate(-1)
+        setSelectedTopic({});
+        setShowInformationBox(false);
+    };
 
     return (
         <Topics >
             <h2 className="title-goals">Personal Goals</h2>
 
-            <TopicCardsView goals={goals} />
+            <Cards>
+                {goals?.map(g => <TopicCards key={g._id} {...g} handleLearnMoreClick={handleLearnMoreClick} />)}
+            </Cards>
 
+            <InformationBox isVisible={showInformationBox} topic={selectedTopic} onClose={handleCloseBox} />
         </Topics>
     )
 }
