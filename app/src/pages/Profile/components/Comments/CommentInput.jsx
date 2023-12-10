13b84a6b2267764ec330c409f2d6ecda54d6d75b
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-10 04:47:46 +0200

    refactor: Improve CommentInputComponent for better maintainability and user experience
    The component now uses the useCommentsContext hook to access the necessary context, reducing dependencies. Additionally, the onSubmitComment function has been updated to handle replies more efficiently and reset the form after submission, providing a better user experience.

--- app/src/pages/Profile/components/Comments/CommentInput.jsx
+++ app/src/pages/Profile/components/Comments/CommentInput.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import { CommentInput, CommentInputContainer, CommentSubmitButton } from "../../../../styles/ViewsStyles/ProfileStyle/Comments.style";
+import { CommentInput, CommentInputContainer, CommentSubmitButton } from "../../../../styles/ViewsStyles/FeaturesStyle/Comments/CommentForm.style";
+import { useCommentsContext } from "../../../../contexts/comments";
 
-export default function ComponentInputComponent({ addComment }) {
+export default function ComponentInputComponent() {
 
+    const { addComment, onClickResetForm, reply } = useCommentsContext()
     const [comment, setComment] = useState('')
 
     const handleCommentChange = (e) => {
@@ -12,11 +14,20 @@ export default function ComponentInputComponent({ addComment }) {
     async function onSubmitComment(e) {
         e.preventDefault();
 
-        await addComment(comment)
-    }
+        const payload = { comment }
+
+        if (reply.isReply) {
+            payload.parentId = reply.parentId
+        }
+
+        await addComment(payload);
+        onClickResetForm();
+        setComment('');
+    };
+
     return (
         <CommentInputContainer>
-            <h1>Write</h1>
+            {reply.isReply ? <h1>Reply to - {reply.name}</h1> : <h1>Write</h1>}
             <CommentInput
                 value={comment}
                 onChange={handleCommentChange}
