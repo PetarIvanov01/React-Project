2578df7a5ee7b1e4bf346042939cc4ba7b4ceb49
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-10 04:42:29 +0200

    refactor: Improve useComments hook for better performance and readability
    This commit refactors the useComments hook to enhance its performance and readability. Changes include the addition of a loading state, better handling of asynchronous operations, and improved logic for updating the comments state based on the depth of comments. The refactored hook uses a more concise and modular structure.

--- app/src/hooks/useComments.jsx
+++ app/src/hooks/useComments.jsx
@@ -1,39 +1,103 @@
 import { useParams } from "react-router-dom";
 import useErrorBoundryAsync from "./useErrorBoundryAsync";
 import { useCallback, useEffect, useState } from "react";
-import { getComments, postComment } from "../api/services/goalsApi";
 import { useAuth } from "../contexts/auth";
+
+import { getComments, postComment } from "../api/services/goalsApi";
 
 export default function useComments() {
 
     const throwToErrBoundry = useErrorBoundryAsync();
     const { user } = useAuth();
     const { postId } = useParams();
+    const [loading, setLoading] = useState(false);
 
     const [comments, setComments] = useState([]);
 
     useEffect(() => {
+        let isMounted = true;
+
+        setLoading(true);
 
         getComments(postId)
-            .then(setComments)
-            .catch(throwToErrBoundry)
+            .then((data) => {
+                if (isMounted) {
+                    setComments(data);
+                    setLoading(false);
+                }
+            })
+            .catch((err) => {
+                if (isMounted) {
+                    setLoading(false);
+                    throwToErrBoundry(err);
+                }
+            });
 
+        return () => {
+            isMounted = false;
+        };
     }, [throwToErrBoundry, postId]);
 
-    const createComment = useCallback(async (comment) => {
+    const createComment = useCallback(async (data) => {
 
         const payload = {
-            message: comment,
+            message: data.comment,
             userId: user.id,
-            parentId: null
+            parentId: data.parentId || null
         }
-        const created = await postComment(postId, payload);
-        setComments((state) => [...state, created.comment])
+
+        const { comment } = await postComment(postId, payload);
+
+        setComments((state) => {
+
+            if (comment.depth === 1) {
+                return [...state, comment];
+            }
+
+            else if (comment.depth === 2) {
+                const updatedState = state.map((c) => {
+                    if (c._id === comment.parent) {
+
+                        return { ...c, children: [comment, ...(c.children || [])] };
+                    } else if (c.children) {
+
+                        return { ...c, children: c.children.map((child) => child._id === comment.parent ? { ...child, children: [comment, ...(child.children || [])] } : child) };
+                    }
+                    return c;
+                });
+                return updatedState;
+            }
+            else if (comment.depth === 3) {
+
+                const updatedState = state.map((c) => {
+
+                    if (c.children) {
+                        return {
+                            ...c,
+                            children: c.children.map((child) => {
+                                if (child._id === comment.parent) {
+
+                                    return { ...child, children: [comment, ...(child.children || [])] };
+                                } else if (child.children) {
+                                    return { ...child, children: child.children.map((grandchild) => grandchild._id === comment.parent ? { ...grandchild, children: [comment, ...(grandchild.children || [])] } : grandchild) };
+                                }
+                                return child;
+                            }),
+                        };
+                    }
+                    return c;
+                });
+                return updatedState;
+            }
+
+            return state;
+        });
 
     }, [user.id, postId]);
 
     return {
         comments,
-        addComment: createComment
+        addComment: createComment,
+        loading
     }
 }
