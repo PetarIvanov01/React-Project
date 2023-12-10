cdbed06e52144a0db92265b0de87e702adbb179c
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-10 04:45:52 +0200

    refactor: Improve CommentContainerList component for better readability and maintainability
    This commit refactors the CommentContainerList component for better readability and maintainability. The component now uses a separate CommentTree component for rendering individual comments.

--- app/src/pages/Profile/components/Comments/CommentContainer.jsx
+++ app/src/pages/Profile/components/Comments/CommentContainer.jsx
@@ -1,169 +1,33 @@
-import {
-    CommentsList,
-    CommentContainer,
-    CommentItem,
-    CommentAvatar,
-    CommentBox,
-    CommentContent,
-    CommentHead,
-    CommentMainLevel,
-    CommentName,
-    CommentTimeStatus
-} from "../../../../styles/ViewsStyles/ProfileStyle/Comments.style";
-import { avatars } from '../../../../util/_mockAvatars.js'
-import transformDate from "../../../../util/transformDate.js";
+import React from "react";
+import { CommentContainer } from "../../../../styles/ViewsStyles/FeaturesStyle/Comments/CommentsList.style.js";
+import MemoReusableComment from "./CommentTree.jsx";
+import { useCommentsContext } from "../../../../contexts/comments.jsx";
 
-const sampleComments = [
-    {
-        id: 1,
-        avatar: avatars[0].value,
-        author: 'petar',
-        time: '20 minutes ago',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?',
-    },
-    {
-        id: 2,
-        avatar: avatars[1].value,
-        author: 'stoyan',
-        time: '15 minutes ago',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?',
-    },
-    {
-        id: 3,
-        avatar: avatars[2].value,
-        author: 'emcho',
-        time: '12 minutes ago',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?',
-    },
-    {
-        id: 4,
-        avatar: avatars[3].value,
-        author: 'dani',
-        time: '7 minutes ago',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?',
-    }, {
-        id: 5,
-        avatar: avatars[0].value,
-        author: 'Kiro Breika',
-        time: '20 minutes ago',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?',
-    },
-    {
-        id: 6,
-        avatar: avatars[1].value,
-        author: 'Minko Lajnata',
-        time: '15 minutes ago',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?',
-    },
-    {
-        id: 7,
-        avatar: avatars[2].value,
-        author: 'Stoyan Kukov',
-        time: '12 minutes ago',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?',
-    },
-    {
-        id: 8,
-        avatar: avatars[3].value,
-        author: 'Pesho Ribata',
-        time: '7 minutes ago',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?',
-    },
-];
+function CommentContainerList({
+    user,
+}) {
+    const {
+        comments,
+        onClickResetForm,
+        onClickSetReplyForm,
+        isReplyOnParrent
+    } = useCommentsContext();
 
-export default function CommentContainerList({ comments, user }) {
-
-    function RecursiveChildren(comment) {
-
-        if (Object.keys(comment).length === 0) {
-            return;
-        }
-
-        // console.log(comment.ownerIdProfile?.username);
-        // console.log(comment.depth > 1);
-        return (
-            <Test
-                key={comment._id}
-                comment={comment}
-                RecursiveChildren={RecursiveChildren}
-                isAuthor={(comment.ownerId === user?.id).toString()}
-                isReply={(comment.depth > 1).toString()}
-            />
-        )
-
-    }
 
     return (
-        <CommentContainer>
-
-            {/* {sampleComments.map((comment, index) => (
-                    <Comment key={comment.id}
-                        comment={comment}
-                        isAuthor={(index % 2 === 0).toString()}
-                        isReply={(index > 0 && index % 2 !== 0).toString()} />
-                ))} */}
-            {comments.map(RecursiveChildren)}
-
-
+        <CommentContainer >
+            {comments.map((comment) =>
+                <MemoReusableComment
+                    userId={user.id}
+                    key={comment._id}
+                    comment={comment}
+                    isAuthor={(comment.ownerId === user?.id).toString()}
+                    isReply={(comment.depth > 1).toString()}
+                    isReplyOnParrent={isReplyOnParrent}
+                    onClickSetReplyForm={onClickSetReplyForm}
+                    onClickResetForm={onClickResetForm}
+                />)}
         </CommentContainer>
-    )
-}
-
-const Test = ({ comment, RecursiveChildren, isAuthor, isReply }) => (
-    <CommentsList >
-        <CommentItem>
-
-            <CommentMainLevel>
-
-                <CommentAvatar $isreply={isReply}>
-                    <img src={comment.ownerIdProfile?.avatarImg.value} alt={comment.ownerIdProfile?.avatarImg.label} />
-                </CommentAvatar>
-
-                <CommentBox $isreply={isReply}>
-
-                    <CommentHead $isreply={isReply}>
-
-                        <CommentName $isauthor={isAuthor}>
-                            <span>{comment.ownerIdProfile?.username}</span>
-                        </CommentName>
-
-                        <CommentTimeStatus>
-                            <span>{transformDate(comment.createdAt)}</span>
-                            <img src='/imgs/Icons/notLiked.png' className="heart" alt="notLiked" />
-                        </CommentTimeStatus>
-
-                    </CommentHead>
-
-                    <CommentContent $isreply={isReply}>{comment.message}</CommentContent>
-
-                    {comment.children.map(RecursiveChildren)}
-                </CommentBox>
-
-            </CommentMainLevel>
-
-        </CommentItem>
-    </CommentsList>
-
-)
-
-const Comment = ({ comment, isAuthor, isReply }) => (
-    <CommentItem>
-        <CommentMainLevel>
-            <CommentAvatar $isreply={isReply}>
-                <img src={comment.avatar} alt="" />
-            </CommentAvatar>
-            <CommentBox $isreply={isReply}>
-                <CommentHead $isreply={isReply}>
-                    <CommentName $isauthor={isAuthor}>
-                        <span>{comment.author}</span>
-                    </CommentName>
-                    <CommentTimeStatus>
-                        <span>{comment.time}</span>
-                        <img src='/imgs/Icons/notLiked.png' className="heart" alt="notLiked" />
-                    </CommentTimeStatus>
-                </CommentHead>
-                <CommentContent $isreply={isReply}>{comment.content}</CommentContent>
-            </CommentBox>
-        </CommentMainLevel>
-    </CommentItem>
-);
+    );
+};
+export default React.memo(CommentContainerList);
